caff2e5ede3c172fe928d19deed2617b8a84c24b
Vinay Chitrakathi <chitrakathi03@gmail.com>
2018-08-12 22:13:34 +0530

    Updated

---
+++ CsharpIndxr/CsharpAssoc/ExecIndexer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAssoc
+{
+    class ExecIndexer
+    {
+        static void Main(string[] args)
+        {
+            var name = new IndexerClass();
+            name[0] = "Vinay";
+            name[1] = "Vishal";
+            name[2] = "Viraj";
+            name[3] = "luv";
+
+            for (int i=0; i < 4; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+    }
+}
---
+++ CsharpIndxr/CsharpAssoc/IndexerClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAssoc
+{
+    class IndexerClass
+    {
+        public string[] name;
+        internal int Id = 10;
+        public int num = 20;
+
+        public IndexerClass()
+        {            
+            name = new string[4];
+        }
+
+        public string this[int i]
+        {
+            get
+            {
+                return name[i];
+            }
+
+            set
+            {
+                name[i] = value;
+            }
+        }
+    }
+}
